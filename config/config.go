8e85c94c8b7119d1c36cd76f507a206fd17eddc4
Huy Le <hle2@gohealth.com>
2025-07-30 14:09:10 +0700

    update mysql config

--- config/config.go
+++ config/config.go
@@ -19,11 +19,11 @@ func ConnectDB() {
 	// port := "3306"
 	// dbname := "customer_db"
 
-	user := os.Getenv("MYSQLUSER")
-	password := os.Getenv("MYSQLPASSWORD")
-	host := os.Getenv("MYSQLHOST")
-	port := os.Getenv("MYSQLPORT")
-	dbname := os.Getenv("MYSQLDATABASE")
+	user := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	dbname := os.Getenv("MYSQL_DATABASE")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, password, host, port, dbname)
